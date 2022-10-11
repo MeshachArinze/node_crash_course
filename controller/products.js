fb6331bea98e5588814fdaac050d2c8b0f96b958
MeshachArinze <meshacharinze@gmail.com>
2022-10-11 13:31:56 +0100

    first commit

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,6 @@
 const products = [];
 
-exports.getProduct = (req, res, next) => {
+exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
@@ -15,4 +15,16 @@ exports.postAddProduct = (req, res, next) => {
         title: req.body.title
     });
     res.redirect('/');
+};
+
+
+exports.getProduct = (req, res, next) => {
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const productController = require('../controller/products')
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productController.getProduct ) 
+router.get('/add-product', productController.getAddProduct ) 
 
 // /admin/add-product => POST
 router.post('/add-product', productController.postAddProduct);
