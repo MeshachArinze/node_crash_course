6b8dee4093d91aa568460f56dc9f5d50700e265e
MeshachArinze <meshacharinze@gmail.com>
2022-10-25 00:20:36 +0100

    refactor to my github

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+const port = 3001;
+const hostname = '127.0.0.1';
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const get404 = require('./controller/erroe404');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(get404.get404);
+app.use(errorController.get404);
 
-app.listen(3001);
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.listen(port);
--- routes/shop.js
+++ routes/shop.js
@@ -1,18 +1,23 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-// const rootDir = require("../util/path");
-// const adminData = require("./admin");
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-const shopController = require("../controller/shop");
+router.get('/', shopController.getIndex);
 
-router.get("/", shopController.getIndex);
+router.get('/products', shopController.getProducts);
 
-router.get("/products",  shopController.getProduct);
-router.get("/cart", shopController.getCart);
-router.get("/checkout", shopController.getCheckout);
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/cart', shopController.postCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
